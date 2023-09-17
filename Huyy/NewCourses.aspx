2e3b2d7ea78ca0555c4ef597536a6567b643c131
huy <huy@mail>
2023-09-17 13:32:31 +0700

    Add new courses

--- Huyy/NewCourses.aspx
+++ Huyy/NewCourses.aspx
@@ -1,3 +1,30 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="NewCourses.aspx.cs" Inherits="Huyy.NewCourses" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div class="add_container">
+        <form method = "post" class="form-container">
+        <div class="form-group">
+            <label for="name">Tên khóa học</label>
+            <input type="text" class="form-control" id="name" name ="name" required>
+        </div>
+        <div class="form-group">
+            <label for="description">Mô tả</label>
+            <input type="text" class="form-control" id="description" name ="description" required>
+        </div>
+        <div class="form-group">
+            <label for="name">ID</label>
+            <input type="text" class="form-control" id="id" name ="id" required>
+        </div>
+        <div class="form-group">          
+            <label for="name">Source (Use URL)</label>
+            <input type="text" class="form-control" id="source" name ="source" required>
+        </div>
+        <div class="form-group">
+            <label for="name">slug</label>
+            <input type="text" class="form-control" id="slug" name ="slug" required>
+        </div>
+
+        <input type="submit" name="add" id="add" class="btn-add" value="Add new courses"/>        
+    </form>
+    </div>
+    
 </asp:Content>
--- Huyy/NewCourses.aspx.cs
+++ Huyy/NewCourses.aspx.cs
@@ -4,13 +4,58 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace Huyy
 {
     public partial class NewCourses : System.Web.UI.Page
     {
+        string connectionString = ConfigurationManager.ConnectionStrings["ConnectionStringName"].ConnectionString;
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (Request.Form["add"] != null && Session["login"] == "1")
+            {
+                string name = Request.Form["name"];
+                string description = Request.Form["description"];
+                string id = Request.Form["id"];
+                string source = Request.Form["source"];
+                string slug = Request.Form["slug"];
+
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    // Use a parameterized query to prevent SQL injection
+                    string insertQuery = "INSERT INTO courses (name, description, id, sources, slug) VALUES (@name, @description, @id, @source, @slug)";
+
+                    using (SqlCommand cmd = new SqlCommand(insertQuery, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@name", name);
+                        cmd.Parameters.AddWithValue("@description", description);
+                        cmd.Parameters.AddWithValue("@id", id);
+                        cmd.Parameters.AddWithValue("@source", source);
+                        cmd.Parameters.AddWithValue("@slug", slug);
+
+                        try
+                        {
+                            cmd.ExecuteNonQuery();
+                            Response.Redirect("MyCourses.aspx");
+                        }
+                        catch (Exception ex)
+                        {
+                            // Handle the exception (log, display an error message, etc.)
+                            // For simplicity, we'll just display a generic error message here
+                            Response.Write("An error occurred: " + ex.Message);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                Response.Redirect("Index.aspx");
+            }
+
 
         }
     }
